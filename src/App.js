1531a761a879a2a82b0d72dea7428ebee9eccc83
Slava Guzenko <gslava@gmail.com>
2019-04-22 00:32:18 +1000

    fixed the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Doughnut, Bar } from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
 
-const labels = ['so-so', 'good', 'great']
-const buttons = ['btn-danger', 'btn-warning', 'btn-success']
-const colors = ['#EB3547', '#FFC03E', '#00A553']
+const MOOD = [
+  { id: 'red', label: 'so-so', button: 'btn-danger', color: '#EB3547' },
+  { id: 'yellow', label: 'good', button: 'btn-warning', color: '#FFC03E' },
+  { id: 'green', label: 'great', button: 'btn-success', color: '#00A553' },
+]
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -14,134 +16,82 @@ const headers = {
 }
 
 const postMood = async mood => {
-  const response = await fetch('apiUrl', {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(mood),
-  })
-  const content = await response.json()
-  console.log(content)
-  return content
+  try {
+    console.log('Posting mood', mood)
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(mood),
+    })
+    const content = await response.json()
+    return content
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 const getMood = async () => {
-  const response = await fetch('https://httpbin.org/get', {
-    headers: {
-      Accept: 'application/json',
-    },
-  })
-  const content = await response.json()
-  console.log(content)
-  return content
+  try {
+    const response = await fetch(apiUrl, { headers })
+    const content = await response.json()
+    return content
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 const Options = ({ onSelect }) => (
   <div>
-    {buttons.map((btn, i) => (
+    {MOOD.map(m => (
       <button
-        key={labels[i]}
-        className={`btn ${btn} btn-lg m-2`}
-        onClick={() => onSelect(i)}
+        key={m.id}
+        className={`btn ${m.button} btn-lg m-2`}
+        onClick={() => onSelect(m.id)}
       >
-        {labels[i]}
+        {m.label}
       </button>
     ))}
   </div>
 )
 
-const Report = ({ answers }) => (
+const Chart = ({ mood }) => (
   <Doughnut
     height={120}
     options={{ maintainAspectRatio: false, legend: false, rotation: 1.57 }}
-    data={{ labels, datasets: [{ data: answers, backgroundColor: colors }] }}
-  />
-)
-
-const options = {
-  responsive: true,
-  tooltips: {
-    mode: 'label',
-  },
-  elements: {
-    line: {
-      fill: false,
-    },
-  },
-}
-
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'Sales',
-      type: 'line',
-      data: [51, 65, 40, 49, 60, 37, 40],
-      fill: false,
-      borderColor: '#EC932F',
-      backgroundColor: '#EC932F',
-      // pointBorderColor: '#EC932F',
-      // pointBackgroundColor: '#EC932F',
-      // pointHoverBackgroundColor: '#EC932F',
-      // pointHoverBorderColor: '#EC932F',
-      // yAxisID: 'y-axis-2'
-    },
-    {
-      type: 'line',
-      label: 'Visitor',
-      data: [200, 185, 590, 621, 250, 400, 95],
-      fill: false,
-      backgroundColor: '#71B37C',
-      borderColor: '#71B37C',
-      // hoverBackgroundColor: '#71B37C',
-      // hoverBorderColor: '#71B37C',
-      // yAxisID: 'y-axis-1'
-    },
-  ],
-}
-
-const Trend = ({ answers }) => (
-  <Bar
-    data={data}
-    height={200}
-    options={{
-      maintainAspectRatio: false,
-      legend: false,
-      scales: {
-        yAxes: [
-          {
-            // stacked: true,
-          },
-        ],
-      },
+    data={{
+      labels: MOOD.map(m => m.label),
+      datasets: [
+        {
+          data: mood && [mood.red, mood.yellow, mood.green],
+          backgroundColor: MOOD.map(m => m.color),
+        },
+      ],
     }}
   />
 )
 
 class App extends Component {
   state = {
-    answers: [
-      {
-        date: '2019-04-21',
-        red: 0,
-        yellow: 0,
-        green: 0,
-      },
-    ],
+    mood: null,
   }
-  handleSelect = choice => {
-    const answers = [...this.state.answers]
-    answers[choice]++
-    this.setState({ answers })
+  async componentDidMount() {
+    const mood = await getMood()
+    this.setState({ mood })
+  }
+  handleSelect = async moodId => {
+    const mood = { red: 0, yellow: 0, green: 0 }
+    mood[moodId] = 1
+    const updatedMood = await postMood(mood)
+    this.setState({ mood: updatedMood })
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header" style={{ paddingBottom: '250px' }}>
+        <header className="App-header">
           <p>How do you feel today?</p>
           <Options onSelect={this.handleSelect} />
           <div className="fixed-bottom">
-            {/* <Report answers={this.state.answers} /> */}
-            <Trend answers={this.state.answers} />
+            <Chart mood={this.state.mood} />
           </div>
         </header>
       </div>
