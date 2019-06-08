1c765a69f4e1bc6222f9456c530670fa778e826c
Slava Guzenko <gslava@gmail.com>
2019-06-08 16:27:53 +1000

    using HashRouter to fix routing on github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Vote from './pages/Vote'
 import Report from './pages/Report'
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={Vote} />
       <Route path="/report" component={Report} />
       <Redirect from="*" to="/" />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 export default App
