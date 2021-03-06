f69c37cbcc5d69176517a8b230ec90111ba368ce
Slava Guzenko <gslava@gmail.com>
2021-03-06 21:13:14 +1100

    fixed unit tests

--- src/views/Home.test.js
+++ src/views/Home.test.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './Home'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 it('renders', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Home />, div)
+  ReactDOM.render(
+    <Router>
+      <Home />
+    </Router>,
+    div
+  )
 })
